6e693c1b79843bb6370b6b10a67d970f807810df
arkk200 <arkk20000@gmail.com>
2025-07-30 18:03:25 +0900

    temp(swea): 2383 code

--- swea/2383/code.py
+++ swea/2383/code.py
@@ -5,6 +5,7 @@ for t in range(T):
     N = int(input())
 
     pos_list = []
+    stairs_number_list = []
     stairs_list = []
     
     for i in range(N):
@@ -13,45 +14,53 @@ for t in range(T):
             if cell == 1:
                 pos_list.append((i, j))
             if cell >= 2:
+                stairs_number_list.append(cell)
                 stairs_list.append((i, j))
     
     def get_dist(A: Tuple[int, int], B: Tuple[int, int]):
         return abs(A[0] - B[0]) + abs(A[1] - B[1])
     
-    def f(stairs_orders, step):
+    def brute_force_stairs(stairs_orders, step):
         if step == N:
-            first_stairs_distance_list = []
-            second_stairs_distance_list = []
+            stairs_distances = [
+                [],
+                []
+            ]
             
             for i, stairs_order in enumerate(stairs_orders):
-                if stairs_order == 0:
-                    first_stairs_distance_list.append(
-                        get_dist(stairs_list[0], pos_list[i])
-                    )
-                else:
-                    second_stairs_distance_list.append(
-                        get_dist(stairs_list[1], pos_list[i])
-                    )
-            first_stairs_distance_list.sort()
-            second_stairs_distance_list.sort()
-            print(stairs_orders)
-            print(first_stairs_distance_list)
-            print(second_stairs_distance_list)
+                stairs_distances[stairs_order].append(
+                    get_dist(stairs_list[stairs_order], pos_list[i])
+                )
+
+            stairs_distances[0].sort()
+            stairs_distances[1].sort()
+            print(stairs_distances[0])
+            print(stairs_distances[1])
 
             spend_time = 0
-            first_stairs_queue = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
-            second_stairs_queue = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
-
-            i = 0
-            j = 0
+            # 계단에 있는 사람 수
+            first_stairs_people_number = 0
+            second_stairs_people_number = 0
+            t = 0
 
             while True:
-                min_t = min(first_stairs_distance_list[i], second_stairs_distance_list[j])
-                spend_time += min_t
-                pass
+                t += 1
+
+                first_stairs_distances = stairs_distances[0]
+
+                for i in range(len(first_stairs_distances)):
+                    if (first_stairs_distances[i] - 1 == 0 and
+                        first_stairs_people_number < stairs_number_list[0]):
+                        pass
+                    
+                    first_stairs_distances[i] -= 1
+                    if first_stairs_distances[i] == 0:
+                        first_stairs_people_number += 1
+                
             return
-        f(stairs_orders + [0], step + 1)
-        f(stairs_orders + [1], step + 1)
+        
+        brute_force_stairs(stairs_orders + [0], step + 1)
+        brute_force_stairs(stairs_orders + [1], step + 1)
         pass
-    f([0], 1)
-    f([1], 1)
+    brute_force_stairs([0], 1)
+    brute_force_stairs([1], 1)
