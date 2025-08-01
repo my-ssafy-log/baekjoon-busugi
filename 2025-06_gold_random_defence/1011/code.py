65317b9d9a98dedd6dc6005b49a9f578f8ac77da
arkk200 <arkk20000@gmail.com>
2025-08-01 15:12:44 +0900

    code: Add implementation for problem 1011

--- 2025-06_gold_random_defence/1011/code.py
+++ 2025-06_gold_random_defence/1011/code.py
@@ -2,6 +2,7 @@ import math
 import sys
 
 def readline():
+    # return input()
     return sys.stdin.readline().rstrip()
 
 T = int(readline())
@@ -9,18 +10,18 @@ T = int(readline())
 for t in range(T):
     x, y = map(int, readline().split())
 
-    min_step = math.inf
-
-    def f(k, d, step):
-        global min_step
-        if d < 0: return None
-        if d == 0:
-            if k == 1:
-                min_step = min(min_step, step)
-            return None
-        if k - 1 >= 1:
-            f(k-1, d - (k-1), step + 1)
-        if k >= 1: f(k, d-k, step + 1)
-        f(k+1, d - (k+1), step + 1)
-    f(0, y-x, 0)
-    print(min_step)
+    t = 0
+    k = 1
+    d = y - x
+    while True:
+        # print(k, end=' ')
+        d -= k
+        t += 1
+        if d == 0: break
+        if d >= (k+1) * (k+2) // 2:
+            k += 1
+        elif d >= k * (k+1) // 2:
+            pass
+        else:
+            k -= 1
+    print(t)
