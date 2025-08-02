bea3abe58a67a59330da5f553612678078fdb64a
arkk200 <arkk20000@gmail.com>
2025-08-02 14:19:59 +0900

    code(swea): Implement problem 5653

--- swea/5653/code.py
+++ swea/5653/code.py
@@ -1,13 +1,14 @@
 T = int(input())
-
+ 
 for t in range(T):
     N, M, K = map(int, input().split())
-
-    origin_map: dict[tuple[int, int], int] = {}
+ 
+    dead_set = set()
+    origin_map = {}
     simul_map = {}
-
+ 
     alive = 0
-
+ 
     for i in range(N):
         row = list(map(int, input().split()))
         for j in range(M):
@@ -15,34 +16,42 @@ for t in range(T):
                 alive += 1
                 origin_map[(i, j)] = row[j]
                 simul_map[(i, j)] = row[j]
-
+ 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-
+ 
     next_simul_map = {}
-
+ 
     def bunsik(pos):
-        global next_simul_map
+        global next_simul_map, alive
         x, y = pos
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
+            if (nx, ny) in dead_set: continue
             if (nx, ny) in simul_map: continue
-            
+            if (nx, ny) not in next_simul_map: alive += 1
             next_mic = max(next_simul_map.get((nx, ny), 0), origin_map[(x, y)])
             origin_map[(nx, ny)] = next_mic
             next_simul_map[(nx, ny)] = next_mic
-
+ 
     for _ in range(K):
         next_simul_map = simul_map.copy()
         for pos in simul_map:
-            simul_map[pos] -= 1
-            if simul_map[pos] == 0:
+            if pos in dead_set:
+                continue
+ 
+            if next_simul_map[pos] != -origin_map[pos]:
+                next_simul_map[pos] -= 1
+ 
+            if next_simul_map[pos] == -1:
                 bunsik(pos)
-            elif simul_map[pos] == -origin_map[pos]:
+ 
+            if next_simul_map[pos] == -origin_map[pos]:
+                dead_set.add(pos)
+                del(origin_map[pos])
+                del(next_simul_map[pos])
                 alive -= 1
-        print("origin_map")
-        print(origin_map)
-        print("simul_map")
-        print(simul_map)
+ 
         simul_map = next_simul_map
+    print(f"#{t+1} {alive}")
