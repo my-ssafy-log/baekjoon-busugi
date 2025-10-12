74c67b9feccbfd2063ce20b2270222525ad0234a
arkk200 <arkk20000@gmail.com>
2025-10-12 23:39:57 +0900

    code: 백준 14002 가장 긴 증가하는 부분 수열 4

---
+++ baekjoon-steps/dp_and_shortest_path_reconstruction/14002/code.py
@@ -0,0 +1,25 @@
+from bisect import bisect_left
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0]
+d = [0] * N
+for i, v in enumerate(arr):
+    length = bisect_left(dp, v)
+    if len(dp) == length:
+        dp.append(v)
+    else:
+        dp[length] = v
+    d[i] = length
+
+max_idx = d.index(max(d))
+path = [arr[max_idx]]
+cur_idx = max_idx
+for idx in range(max_idx, -1, -1):
+    if d[idx] == d[cur_idx] - 1:
+        cur_idx = idx
+        path.append(arr[idx])
+
+print(len(path))
+print(*path[::-1])
